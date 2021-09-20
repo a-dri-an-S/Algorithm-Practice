7a94847baff98e5646ed9360f767b47d866a394f
Adrian Salinas <adrian.salinas.1986@gmail.com>
2021-09-20 16:31:19 -0700

    Final Value of Variable After Performing Operations - One Liner

--- finalValueAfterOperations.js
+++ finalValueAfterOperations.js
@@ -14,3 +14,12 @@ const finalValueAfterOperations = function(operations, x = 0) {
 // 1. Use forEach() methd to iterate through operations array
 // 2. If current op includes "-", decrement x by 1, else increment x by 1
 // 3. Return x
+
+// One-Liner
+var finalValueAfterOperations = (operations) => operations.map(op => {
+    return op.includes("-") ? -1 : 1;
+}).reduce((a, b) => a + b, 0)
+
+// 1. Itereate and mutate operations array with map()
+//  - If current "op" includes "-" return -1, else 1
+// 2. Sum array with reduce() method to return number
