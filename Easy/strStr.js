2f6a6bef5256c8c39328f098898039beb2f6e24e
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-10-25 19:02:11 -0700

    Implement strStr()

---
+++ Easy/strStr.js
@@ -0,0 +1,7 @@
+// Implement strStr().
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+const strStr = (haystack, needle) => haystack.indexOf(needle);
+
+// 1. Find the index of the first instance of needle in haystack
+// - Use indexOf to find the index of needle in haystack
