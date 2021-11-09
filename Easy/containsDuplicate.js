2feb625289b3b280e2dbe20b088306a4cbd1d16a
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-11-09 15:21:48 -0800

    Contains Duplicate - Set Solution

--- Easy/containsDuplicate.js
+++ Easy/containsDuplicate.js
@@ -7,3 +7,21 @@ const containsDuplicate = function(nums) {
 // 1. Place nums in Set to return unique characters, spread into an array
 // 2. Get length of array and check if its not equal to length of nums
 // 3. Return boolean
+
+// Set Object Solution
+const containsDuplicate = function(nums) {
+    const set = new Set();
+    
+    for (const num of nums) {
+        if (set.has(num)) return true;
+        set.add(num);
+    }
+    return false;
+};
+
+// 1. Declare set and set it to an empty Set() object
+// 2. Use for loop to iterate through nums array
+//  - If set contains current num (value at current position in array), return true
+//      - Use has() method to check if set contains current value
+//  - Add num (value at current position) to set with add() method
+// 3. If loop ends and there are no duplicates, return false
