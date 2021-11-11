fcfa7117f4715e54f4e80228af9cac3109dd1f08
a-dri-an-S <adrian.salinas.1986@gmail.com>
2021-11-11 10:45:14 -0800

    Intersection of Two Arrays II

--- Easy/intersect.js
+++ Easy/intersect.js
@@ -18,3 +18,36 @@ const intersect = (nums1, nums2) => {
 //  - Push current n1 into arr array
 //  - Remove current n1 in nums2 via splice method by getting index of n1 inside of nums2
 // 4. Return arr
+
+// HASHMAP SOLUTION
+
+const intersect = function(nums1, nums2) {
+    let hash = {};
+    let arr = [];
+    
+    for (const num of nums1) {
+        if (!hash[num]) {
+            hash[num] = 0;
+        }
+        hash[num]++;
+    }
+    
+    for (const num of nums2) {
+        if (hash[num] > 0) {
+            arr.push(num);
+            hash[num]--;
+        }
+    }
+    return arr;
+};
+
+// 1. Declare hash and set it to an empty object
+// 2. Declare arr and set it to an empty array
+// 3. Use for loop to iterate through nums1 array
+//  - If current value (num) is not in hash object (is false), add num to hash and set its value to 0
+//  - Increment num in hash by 1
+// 4. Use for loop to iterate through nums2 array
+//  - If value of current num in hash object is greater than 0
+//      - Append num to arr array with push() method
+//      - Decrement num is hash by 1
+// 5. Return arr
