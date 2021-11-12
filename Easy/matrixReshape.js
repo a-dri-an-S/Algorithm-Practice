c4d7c443bcd8749fe6820591ca7e863fa4815396
a-dri-an-S <adrian.salinas.1986@gmail.com>
2021-11-11 21:46:41 -0800

    Reshape the Matrix - 2nd Solution

--- Easy/matrixReshape.js
+++ Easy/matrixReshape.js
@@ -23,3 +23,39 @@ const matrixReshape = function(mat, r, c) {
 //  - Use for loop to create a loop based off of amount of rows
 //  - Splice from 0 to column amount (c) in flatten array and push into array
 // 4. Return array 
+
+// Solution #2
+var matrixReshape = function(mat, r, c) {
+    const cells = mat.flat();
+    if (cells.length !== r * c) return mat;
+    
+    const matrix = [];
+    let idx = 0;
+    
+    for (let i = 0; i < r; i++) {
+        let col = 0;
+        let row = [];
+
+        while (col < c) {
+            row.push(cells[idx]);
+            idx++;
+            col++;
+        }
+        matrix.push(row);
+    }
+    
+    return matrix;
+};
+
+// 1. Declare cells and set it to mat matrix transformed into a single array with flat() method
+// 2. If length of cells is not equal to the product of r and c, return mat
+// 3. Declare matrix and set it to an empty array
+// 4. Declare idx and set it to 0
+// 5. Use for loop to create loop for making rows
+//  - Declare col and set it to 0
+//  - Declare row and set it to an empty array
+// 6. Use a while loop to create loop until col is greater than or equal to c
+//  - Append value at index-idx of cells array into arr array with push() method;
+//  - Increment idx and col by 1
+// 7. When while loop ends, Append row array to matrix array with push() method
+// 8. When for loop ends, return matrix
