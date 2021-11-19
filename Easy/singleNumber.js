68f47c9dc3c5e92de707bdabd526a880ec44f2c7
a-dri-an-S <adrian.salinas.1986@gmail.com>
2021-11-18 19:04:43 -0800

    Single Number SOLUTION #2

--- Easy/singleNumber.js
+++ Easy/singleNumber.js
@@ -19,3 +19,21 @@ const singleNumber = nums => {
 // 3. If num is not found in object, add it to object and set it to 0
 // 4. Increment num in counts object by 1
 // 5. Extract key value pairs from counts and sort the array by increasing values, Return key at [0][0]
+
+// SOLUTION #2
+const singleNumber = function(nums) {
+    nums.sort();
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i + 1]) {
+            i++;
+        } else {
+            return nums[i]
+        }
+    }
+};
+
+// 1. Sort nums in ascending order with sort() method
+// 2. Use for loop to iterate through nums array
+//  - If current value in nums(nums[i]) is equal to next value in nums(nums[i + 1]), increment i by 1 (move up one index since the duplicates come in pairs)
+// - Else, return current value in nums (nums[i])
